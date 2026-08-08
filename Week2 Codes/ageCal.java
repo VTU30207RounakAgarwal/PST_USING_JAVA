c8d2355d4f0b5f450406cdc87bfde343ca76ceee
VTU30207RounakAgarwal <vtu30207@veltech.edu.in>
2026-08-08 08:56:23 +0530

    Added code new file

---
+++ Week2 Codes/ageCal.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.time.LocalDate;
+import java.time.Period;
+public class ageCal {
+    public static void main(String args[]){
+      Scanner sc=new Scanner(System.in);
+        // int currYear=LocalDate.now().getYear();
+        // int currMonth=LocalDate.now().getMonthValue();
+        // System.out.print("Enter your birth year:");
+        // int birthYear=sc.nextInt();
+        // System.out.print("Enter your birth Month");
+        // int age=currYear-birthYear;
+        // System.out.println(age);
+
+        LocalDate birthDate=LocalDate.of(2005,11,02);
+        
+        LocalDate today=LocalDate.now();
+
+        Period age=Period.between(birthDate, today);
+
+        System.out.println("Age "+age.getYears()+" years");
+        System.out.println("Month "+age.getMonths());
+        System.out.println("Day "+age.getDays());
+    }
+}
